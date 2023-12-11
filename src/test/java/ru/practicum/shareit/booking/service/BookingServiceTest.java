ab4ee553dae9a490f7cc575a91876484cf4ef4b1
Ян Борис Сергеевич <mr.boris.yan@yandex.ru>
2023-12-11 23:10:37 +0300

    HW15R1 - test prod debug

--- src/test/java/ru/practicum/shareit/booking/service/BookingServiceTest.java
+++ src/test/java/ru/practicum/shareit/booking/service/BookingServiceTest.java
@@ -7,7 +7,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import ru.practicum.shareit.booking.dto.BookingDtoRequest;
 import ru.practicum.shareit.booking.dto.BookingDtoResponse;
-import ru.practicum.shareit.booking.dto.BookingMapper;
 import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.booking.model.BookingStatus;
 import ru.practicum.shareit.booking.storage.BookingRepository;
