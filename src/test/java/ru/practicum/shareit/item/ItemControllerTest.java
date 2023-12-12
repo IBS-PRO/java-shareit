1b03f44b7883de8783c410a5fcb3deeea97e8416
Ян Борис Сергеевич <mr.boris.yan@yandex.ru>
2023-12-12 23:29:55 +0300

    HW15R1 - fix

--- src/test/java/ru/practicum/shareit/item/ItemControllerTest.java
+++ src/test/java/ru/practicum/shareit/item/ItemControllerTest.java
@@ -15,11 +15,12 @@ import ru.practicum.shareit.item.service.ItemService;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(value = ItemController.class)
@@ -31,6 +32,64 @@ class ItemControllerTest {
     ObjectMapper objectMapper;
     @MockBean
     private ItemService itemServiceMock;
+
+    @SneakyThrows
+    @Test
+    void addItem_itemIsValid_returnValid() {
+        ItemDto item = new ItemDto();
+        item.setId(1L);
+        item.setAvailable(true);
+        item.setName("itemNameTest");
+        item.setDescription("DescriptionTest");
+        item.setComments(new ArrayList<>());
+        Long userId = 1L;
+        when(itemServiceMock.addItem(anyLong(), any())).thenReturn(item);
+
+        String response = mockMvc.perform(post("/items")
+                        .content(new ObjectMapper().writeValueAsString(item))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("X-Sharer-User-Id", userId))
+
+                .andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+        ItemDto actual = objectMapper.readValue(response, ItemDto.class);
+        assertEquals(item, actual);
+        verify(itemServiceMock).addItem(anyLong(), any());
+
+    }
+
+    @SneakyThrows
+    @Test
+    void getItem_itemIdIsCorrect_returnItem() {
+        ItemDto item = new ItemDto();
+        item.setId(1L);
+        item.setName("itemNameTest");
+        item.setDescription("DescriptionTest");
+        when(itemServiceMock.getItem(any(), any())).thenReturn(item);
+
+        mockMvc.perform(get("/items/{itemId}", item.getId())
+                        .header("X-Sharer-User-Id", item.getId()))
+
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(item.getId()))
+                .andExpect(jsonPath(("$.name")).value(item.getName()))
+                .andExpect(jsonPath("$.description").value(item.getDescription()));
+        verify(itemServiceMock).getItem(any(), any());
+    }
+
+    @SneakyThrows
+    @Test
+    void getItem_itemIdIsIncorrect_returnItem() {
+        ItemDto item = new ItemDto();
+        item.setId(1L);
+        item.setName("itemNameTest");
+        item.setDescription("DescriptionTest");
+        when(itemServiceMock.getItem(any(), any())).thenThrow(NotFoundException.class);
+
+        mockMvc.perform(get("/items/{itemId}", item.getId())
+                        .header("X-Sharer-User-Id", item.getId()))
+                .andExpect(status().isNotFound());
+        verify(itemServiceMock).getItem(any(), any());
+    }
 
     @SneakyThrows
     @Test
